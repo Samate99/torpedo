0cdfedfef805e06207262d713ba810751accc729
Samate99 <mateme@gmail.hu>
2020-05-25 16:27:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# torpedo
+
+The battle ship game.
+
+## Requirements
+
+Building the project requires JDK 11 or later and [Apache Maven](https://maven.apache.org/).
---
+++ src/main/java/torpedo/javafx/Main.java
@@ -0,0 +1,11 @@
+package torpedo.javafx;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(RollingCubesApplication.class, args);
+    }
+
+}
