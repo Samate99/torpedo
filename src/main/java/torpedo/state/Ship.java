67ca1408d287862032d95fc289c7c0a59966516e
Samate99 <43438097+Samate99@users.noreply.github.com>
2020-05-28 12:19:08 +0200

    Update Ship.java

--- src/main/java/torpedo/state/Ship.java
+++ src/main/java/torpedo/state/Ship.java
@@ -13,7 +13,14 @@ public class Ship {
     private int height;
 
     private List<Integer> destroyed;
-
+  /**
+     * Specific of the ship
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param width an integer (the ship width)
+     * @param height an integer (the ship height)
+     */
+    
     public Ship(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
@@ -21,7 +28,12 @@ public class Ship {
         this.height = height;
         this.destroyed = new ArrayList<Integer>();
     }
-
+   /**
+     *This function returns the ship opportunities
+     * @param ship
+     * @param space
+     * @return return the
+     */
     public boolean isOverlap (Ship ship, int space) {
         return x < ship.x + ship.width + space && x + width + space > ship.x && y < ship.y + ship.height + space && y + height + space > ship.y;
     }
